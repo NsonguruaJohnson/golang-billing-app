956d9db9653bdb5a579e9653cf96d2da400c4413
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2021-11-18 20:55:10 +0100

    Lesson three

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Strings
+	// var nameOne string = "Jerry"
+	// var nameTwo = "Johnson"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameOne = "Akpan"
+	// nameThree = "Udo"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "Baba" // used when initializing and declaring a variable
+	// // This declaration cannot be used outside of a function
+
+	// fmt.Println(nameFour)
+
+	// Integers(ints and float)
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// // bits and memory
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint16 = 259
+
+	// Floats
+	var scoreOne float32 = 30.22
+	var scoreTwo float64 = -222.4444433333
+	scoreThree := 56.56
+
+}
