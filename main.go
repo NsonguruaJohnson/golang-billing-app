2e11c66c941df717d4e22a4553c388c78edab7c9
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2023-01-18 20:29:47 +0100

    Completed lesson 7

--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
-	// greeting := "Hello world people"
+	// x := 0
 
-	// fmt.Println(strings.Contains(greeting, "Hello"))
-	// fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-	// fmt.Println(strings.ToUpper(greeting))
-	// fmt.Println(strings.Index(greeting, "w"))
-	// fmt.Println(strings.Split(greeting, "o"))
+	// for x < 5 {
+	// 	fmt.Println("Value of x is:", x)
+	// 	x++
+	// }
 
-	// fmt.Println("Original string is", greeting)
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("The value of i is:", i)
+	// }
 
-	ages := []int{100, 15, 10, 40, 20, 30, 5, 25, 35, 40}
-	sort.Ints(ages)
+	names := []string{"Nsongurua", "Akpan", "Johnson", "Excellent", "Distinguished", "Honoured"}
 
-	fmt.Println(ages)
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
+	// }
 
-	index := sort.SearchInts(ages, 12)
-	fmt.Println(index)
+	// for index, value := range names {
+	// 	fmt.Printf("The position at index %v is %v \n", index, value)
+	// }
 
-	names := []string{"All", "is", "well", "backend", "developer"}
+	for _, value := range names {
+		fmt.Printf("The value is %v \n", value)
+		value = "new string" // This doesn't update the value(Value is a local copy of the variable)
+	}
 
-	sort.Strings(names)
 	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "xyz"))
-	fmt.Println(sort.SearchStrings(names, "develop"))
 
 }
