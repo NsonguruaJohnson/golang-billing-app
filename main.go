655dcdea4253a0c9bc2a417c3f7bfe5fd7c74425
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2023-01-18 12:14:12 +0100

    TO be continued

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	greeting := "Hello world changers"
+	// greeting := "Hello world changers"
 
 	// fmt.Println(strings.Contains(greeting, "Hello"))
 	// fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
 	// fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "w"))
-	fmt.Println(strings.Split(greeting, " "))
+	// fmt.Println(strings.Index(greeting, "w"))
+	// fmt.Println(strings.Split(greeting, "e"))
 
-	fmt.Println("Original string is", greeting)
+	// fmt.Println("Original string is", greeting)
+
+	ages := []int{15, 20, 25, 30, 35, 40, 45, 50, 55}
 }
