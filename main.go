2877cfc2efcd0e7c717e03ccad9db76955d9737c
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2023-01-18 12:34:34 +0100

    Completed lesson 5

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
-	// greeting := "Hello world changers"
+	// var age [3]int = [3]int{5,10,15}
+	var ages = [3]int{10, 20, 30}
 
-	// fmt.Println(strings.Contains(greeting, "Hello"))
-	// fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-	// fmt.Println(strings.ToUpper(greeting))
-	// fmt.Println(strings.Index(greeting, "w"))
-	// fmt.Println(strings.Split(greeting, "e"))
+	names := [4]string{"Nsongurua", "Akpan", "Jimmy", "Johnson"}
+	// fmt.Println(names, len(names))s
 
-	// fmt.Println("Original string is", greeting)
+	names[1] = "Favour"
 
-	ages := []int{15, 20, 25, 30, 35, 40, 45, 50, 55}
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
+
+	// Slices(uses arrays under the hood)
+	var scores = []int{100, 40, 30}
+	scores[2] = 400
+	scores = append(scores, 22)
+
+	fmt.Println(scores, len(scores))
+
+	// slice ranges
+	rangeOne := names[1:3]
+	rangeTwo := names[2:]
+	rangeThree := names[:3]
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "kahoots")
+	fmt.Println(rangeOne)
+
 }
