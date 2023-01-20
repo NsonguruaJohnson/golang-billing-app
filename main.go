246b462b96aa6bec754ce6879cb1b3fd932066e5
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2023-01-20 04:11:26 +0100

    Completed lesson-12

--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import "fmt"
 
-var score = 90.5
-
 func main() {
-	sayHello("Nsongurua")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":          4.99,
+		"pie":           7.99,
+		"salad":         6.99,
+		"toffe pudding": 3.55,
 	}
 
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
 
+	// Looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// int as key type
+	phoneBook := map[int]string{
+		2468: "userOne",
+		1234: "userTwo",
+		3689: "userThree",
+	}
+
+	fmt.Println(phoneBook)
+	fmt.Println(phoneBook[2468])
+
+	phoneBook[1234] = "newUser"
+	fmt.Println(phoneBook)
+
+	phoneBook[3689] = "testUser"
+	fmt.Println(phoneBook)
 }
